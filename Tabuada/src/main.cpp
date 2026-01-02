7716b72768947dca6e7ae11f2223b654fac29d05
Gabriel de Lima Gonçalves <gabrielgonsalves508@gmail.com>
2026-01-02 16:37:33 -0300

    corrigindo frase

--- Tabuada/src/main.cpp
+++ Tabuada/src/main.cpp
@@ -14,7 +14,7 @@ int main() {
         int num = LerInteiro("Digite um número para ver a Tabuada: ");
         Tabuada(num);
 
-        resp = LerString("Deseja digitar outro número? ");
+        resp = LerString("Deseja digitar outro número? [S/N] ");
     } while (toupper(resp[0]) != 'N');
 
     cout << "=== Fim do programa ===" << endl;
