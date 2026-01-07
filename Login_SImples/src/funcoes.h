5d888f2d6fae51207040b450a58ab33f9751b6e4
Gabriel de Lima Gonçalves <gabrielgonsalves508@gmail.com>
2026-01-07 20:36:25 -0300

    adicionando o #ifndef

--- Login_SImples/src/funcoes.h
+++ Login_SImples/src/funcoes.h
@@ -1,4 +1,10 @@
+#ifndef FUNCOES_H // Evitar multiplas inclusões no mesmo Header
+#define FUNCOES_H
+
 #include <string>
 
 // Declarações
 std::string LerString (const std::string& msg);
+
+
+#endif
